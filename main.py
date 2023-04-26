467a35d2e797c7f8cde9e5cc792e2d8c7a1d0165
Gabriel Domene <domenee.g@gmail.com>
2023-04-26 10:25:05 -0300

    other changes

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
-j = [1, 
-     2, 
-     3, 
-     4]
+j = [1, 2, 3, 4]
 
 from random import random, choice
 
 
 def very_important_function(
-    template: str, 
-    file: str, 
-    engine: str, header: bool = True, 
-    debug: bool = False
+    template: str, file: str, engine: str, header: bool = True, debug: bool = False
 ):
     """Applies `variables` to the `template` and writes to `file`."""
     return "something"
 
 
 if __name__ == "__main__":
-    very_important_function("teste", 
-                            "engine_1", "False", 
-                            False, True)
+    very_important_function("teste", "engine_1", "False", False, True)
